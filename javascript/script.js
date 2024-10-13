6a0b4c849b5f60101b23031687b9d2c225c02bc5
MiaTexnes <miatex50428@stud.noroff.no>
2024-10-13 22:26:17 +0200

    small changes all over

--- javascript/script.js
+++ javascript/script.js
@@ -1,39 +1,43 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Card hover effect
     const cards = document.querySelectorAll(".card");
+    if (cards) {
+        cards.forEach((card) => {
+            card.addEventListener("mouseenter", function () {
+                this.style.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.2)";
+            });
 
-    cards.forEach((card) => {
-        card.addEventListener("mouseenter", function () {
-            this.style.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.2)";
+            card.addEventListener("mouseleave", function () {
+                this.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.1)";
+            });
         });
+    }
 
-        card.addEventListener("mouseleave", function () {
-            this.style.boxShadow = "0 4px 6px rgba(0, 0, 0, 0.1)";
-        });
-    });
-});
-document.addEventListener("DOMContentLoaded", function () {
+    // Navbar toggler
     const navbarToggler = document.getElementById("navbar-toggler");
     const navbarNav = document.getElementById("navbarNav");
+    if (navbarToggler && navbarNav) {
+        navbarToggler.addEventListener("click", function () {
+            navbarNav.classList.toggle("show");
+        });
+    }
 
-    navbarToggler.addEventListener("click", function () {
-        navbarNav.classList.toggle("show");
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
+    // Mobile menu button
     const menuButton = document.getElementById("menu-button");
     const mobileMenu = document.getElementById("mobile-menu");
+    if (menuButton && mobileMenu) {
+        menuButton.addEventListener("click", function () {
+            mobileMenu.classList.toggle("hidden");
+        });
+    }
 
-    menuButton.addEventListener("click", function () {
-        mobileMenu.classList.toggle("hidden");
-    });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
+    // Video settings
     const video = document.getElementById("video");
-    video.playbackRate = 1.5; // Adjust the playback speed as needed
-    video.autoplay = true; // Ensure autoplay is enabled
-    video.loop = true; // Ensure looping is enabled
-    video.muted = true; // Mute the video to allow autoplay
-    video.play(); // Start playing the video
+    if (video) {
+        video.playbackRate = 1.5; // Adjust the playback speed as needed
+        video.autoplay = true; // Ensure autoplay is enabled
+        video.loop = true; // Ensure looping is enabled
+        video.muted = true; // Mute the video to allow autoplay
+        video.play(); // Start playing the video
+    }
 });
